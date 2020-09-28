82eb7c65bdb2bd6ab49794b8d1e3fdd72e3a2d77
Daniel-Alencar <danielalencar746@gmail.com>
2020-09-28 11:27:40 -0300

    airline terminado

--- aprendizado/database/passengers/passengers.py
+++ aprendizado/database/passengers/passengers.py
@@ -10,11 +10,28 @@ def main():
 
     # list of all flights
     flights = db.execute("SELECT id, origin, destination, duration FROM flights")
-
     for flight in flights:
         print(f"Flight {flight.id}: {flight.origin} to {flight.destination}, {flight.duration} minutes.")
         
-        
+    # prompt user to coose a flight
+    flight_id = int(input("\nFlight: "))
+    flight = db.execute("SELECT origin, destination, duration FROM flights WHERE id = :id", 
+                        {"id": flight_id}).fetchone()
 
+    # make sure flight is valid
+    if flight is None:
+        print("Error! No such flight.")
+        return
+    
+    # list passengers
+    passengers = db.execute("SELECT name FROM passengers WHERE flight_id = :flight_id", {"flight_id": flight_id}).fetchall()
+
+    print("\nPassengers")
+    for passengers in passengers:
+        print(passenger.name)
+    if len(passengers) == 0:
+        print("No passengers")
+
+        
 if __name__ == "__main__":
     main()
